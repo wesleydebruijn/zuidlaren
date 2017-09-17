d6235a9fa82dfb07beeec119d4e40ef670d728dc
Wesley de Bruijn <wesley.dbr@live.nl>
2017-09-17 14:50:18 +0200

    Add dashboard, teams and user controllers

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,45 @@
+class TeamsController < ApplicationController
+
+  def index
+    @teams = Team.all
+  end
+
+  def new
+    @team = Team.new
+  end
+
+  def edit
+    @team = Team.find(params[:id])
+  end
+
+  def create
+    @team = Team.new(team_params)
+    if @team.save
+      redirect_to teams_path, notice: 'Team is aangemaakt.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    @team = Team.find(params[:id])
+    if @team.update(team_params)
+      redirect_to teams_path, notice: 'Team is aangepast.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @team = Team.find(params[:id])
+    @team.destroy
+    redirect_to teams_path, notice: 'Team is verwijderd.'
+  end
+
+  private
+
+  def team_params
+    params.require(:team).permit(:name)
+  end
+
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path, notice: 'User is aangemaakt.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to users_path, notice: 'User is aangepast.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path, notice: 'User is verwijderd.'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+
+end
