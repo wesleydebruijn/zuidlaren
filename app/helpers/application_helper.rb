6c9c5db8d4863b401fe39c25585615b8009264f3
Wesley de Bruijn <wesley.dbr@live.nl>
2017-09-17 16:32:05 +0200

    Add active class to active navigation items

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,11 @@
+module ApplicationHelper
+
+  def controller?(*controller)
+    controller.include?(params[:controller])
+  end
+
+  def action?(*action)
+    action.include?(params[:action])
+  end
+
+end
