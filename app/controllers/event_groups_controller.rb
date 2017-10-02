c0f10ef26ac3ac46681a136c1add9b0bfc00145b
Wesley de Bruijn <wesley.dbr@live.nl>
2017-10-03 00:23:13 +0200

    Add notifications

--- app/controllers/event_groups_controller.rb
+++ app/controllers/event_groups_controller.rb
@@ -26,7 +26,7 @@ class EventGroupsController < ApplicationController
   def create
     @event_group = EventGroup.new(event_group_params)
     if @event_group.save
-      redirect_to event_groups_path, notice: 'Groep is aangemaakt.'
+      redirect_to event_groups_path, notice: 'Event Group is aangemaakt.'
     else
       render :new
     end
@@ -35,7 +35,7 @@ class EventGroupsController < ApplicationController
   def update
     @event_group = EventGroup.find(params[:id])
     if @event_group.update(event_group_params)
-      redirect_to event_groups_path, notice: 'Groep is aangepast.'
+      redirect_to event_groups_path, notice: 'Event Group is aangepast.'
     else
       render :edit
     end
@@ -44,7 +44,7 @@ class EventGroupsController < ApplicationController
   def destroy
     @event_group = EventGroup.find(params[:id])
     @event_group.destroy
-    redirect_to event_groups_path, notice: 'Groep is verwijderd.'
+    redirect_to event_groups_path, notice: 'Event Group is verwijderd.'
   end
 
   private
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :event_group
   has_and_belongs_to_many :teams
-  has_and_belongs_to_many :events
+  has_and_belongs_to_many :repositories
 end
